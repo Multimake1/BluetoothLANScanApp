23b154ec511c85281f226e5af8a7ffc79479844e
Multimake1 <arsenyi1205@gmail.com>
2026-02-04 15:52:54 +0700

    Initial Commit

---
+++ BluetoothLANScan/BluetoothLANScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothLANScanApp.swift
+//  BluetoothLANScan
+//
+//  Created by Арсений on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothLANScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
